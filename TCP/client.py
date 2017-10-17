6248f7d0412a198869326677f6e6416ddf9baa82
TG <TGMaster@users.noreply.github.com>
2017-10-17 19:26:37 +0700

    add comments

--- TCP/client.py
+++ TCP/client.py
@@ -1,20 +1,20 @@
 import socket
 
-array = ""
-server_address = ('127.0.0.1',12000)
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(server_address)
+array = "" #initialize string
+server_address = ('127.0.0.1',12000) #set ip address and port number to connect to the server
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #use TCP
+clientSocket.connect(server_address) #connect to server
 
 num = input ('Enter how many students: ')
 print ('Enter student name: ')
-for i in range(int(num)):
+for i in range(int(num)): #start loop to enter student name
 	temp = input('Name: ')
 	array += temp + '\n'
 
-message = array.encode()
-clientSocket.sendall(message)
-data = clientSocket.recv(2048)
-message = data.decode()
-print ('Received from server:\n' + message)
+message = array.encode() #in python3 must encode to UTF-8
+clientSocket.sendall(message) #send message to server
+data = clientSocket.recv(2048) #receive message from server
+message = data.decode() #decode it!
+print ('Received from server:\n' + message) #print message
 
-clientSocket.close()
+clientSocket.close() #close connection
