31e160e1f43b7e4a83d4787b8039a87d480754ed
TG <TGMaster@users.noreply.github.com>
2017-10-17 19:40:27 +0700

    add comments

--- TCP/server.py
+++ TCP/server.py
@@ -1,18 +1,18 @@
 import socket
  
-server_address = ('127.0.0.1',12000)
+server_address = ('127.0.0.1',12000) #set ip address and port number for server
     
-serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-serverSocket.bind(server_address)
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #use TCP
+serverSocket.bind(server_address) #assign port number to socket
      
-serverSocket.listen(1)
+serverSocket.listen(1) #The server listen for TCP connection requests from the client.
 print ("The server is ready to receive")
 conn, addr = serverSocket.accept()
 print ("Connection from: " + str(addr))
 while True:
 	data = conn.recv(2048)
 	data = data.decode()
-	if not data:
+	if not data: #if no data receive, break the loop
 		break
 	print ('Uppercase processing...')
 	data = str(data).upper()
